edd5c721260823d1c4a6b191769141ea0bcd53a1
Callum Jules White <62078259+juleswhi@users.noreply.github.com>
2024-03-11 19:37:10 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+﻿# Winforms Form Scraper
+
+A small package to scrape some important control information from forms.
+
+The scraper can either be called on a singular form, or an entire assembly ( Work in progress ) 
---
+++ WinFormsScraper.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+
+namespace WinFormsScraper;
+
+public static class WinFormsScraper
+{
+    public static string Path = "FormDetails.txt";
+    private static List<string> s_printedForms = new();
+    
+    public static void Scrape<T>(T form) where T : Form
+    {
+        using StreamWriter streamWriter = new StreamWriter(Path, true);
+
+        if (s_printedForms.Any(form => form.GetType() == typeof(T)))
+            return;
+
+        streamWriter.WriteLine($"-------------");
+        streamWriter.WriteLine($"{typeof(T).Name}");
+        streamWriter.WriteLine($"-------------");
+        streamWriter.WriteLine($"");
+
+        foreach(Control control in form.Controls)
+        {
+            streamWriter.WriteLine($"{control.Name}:");
+            streamWriter.WriteLine($"\t\tSize: [{control.Size.Width}, {control.Size.Height}]");
+            streamWriter.WriteLine($"\t\tLocation: [{control.Location.X}, {control.Location.Y}]");
+            streamWriter.WriteLine($"\t\tForeColour: {control.ForeColor.Name}");
+            streamWriter.WriteLine($"\t\tBackColour: {control.BackColor.Name}");
+            streamWriter.WriteLine($"\t\tFont: {control.Font.Name}");
+            streamWriter.WriteLine($"\t\tFontSize: {control.Font.Size}");
+            streamWriter.WriteLine();
+        }
+        streamWriter.WriteLine();
+        streamWriter.WriteLine();
+    }
+}
