5da5e9da138d3cb71ae8d6f441a6a1a43e8a7c32
Callum Jules White <62078259+juleswhi@users.noreply.github.com>
2024-03-11 22:58:17 +0000

    v1.1.1

--- WinFormsScraper.cs
+++ WinFormsScraper.cs
@@ -2,35 +2,73 @@
 
 namespace WinFormsScraper;
 
+public enum ScrapeType
+{
+    IMPORTANT,
+    // ALL
+}
+
+/// <summary>
+/// Deals with scraping property data from Forms
+/// </summary>
 public static class WinFormsScraper
 {
     public static string Path = "FormDetails.txt";
-    private static List<string> s_printedForms = new();
+
+    public static ScrapeType Type = ScrapeType.IMPORTANT;
     
+    private static HashSet<string> s_printedForms = new();
+    
+    /// <summary>
+    /// Pretty-Prints all the scraped data to file
+    /// </summary>
+    /// <typeparam name="T">The Type of form</typeparam>
+    /// <param name="form">The Form to scrape</param>
     public static void Scrape<T>(T form) where T : Form
     {
+        if(!File.Exists(Path))
+        {
+            File.Create(Path);
+        }
         using StreamWriter streamWriter = new StreamWriter(Path, true);
 
-        if (s_printedForms.Any(form => form.GetType() == typeof(T)))
+        // Could potentially update the value here
+        if (s_printedForms.Any(formName => formName == typeof(T).Name))
             return;
 
-        streamWriter.WriteLine($"-------------");
-        streamWriter.WriteLine($"{typeof(T).Name}");
-        streamWriter.WriteLine($"-------------");
-        streamWriter.WriteLine($"");
+        s_printedForms.Add(typeof(T).Name);
+
+        streamWriter?.WriteLine($"--- {typeof(T).Name} ---");
+        streamWriter?.WriteLine($"");
 
         foreach(Control control in form.Controls)
         {
-            streamWriter.WriteLine($"{control.Name}:");
-            streamWriter.WriteLine($"\t\tSize: [{control.Size.Width}, {control.Size.Height}]");
-            streamWriter.WriteLine($"\t\tLocation: [{control.Location.X}, {control.Location.Y}]");
-            streamWriter.WriteLine($"\t\tForeColour: {control.ForeColor.Name}");
-            streamWriter.WriteLine($"\t\tBackColour: {control.BackColor.Name}");
-            streamWriter.WriteLine($"\t\tFont: {control.Font.Name}");
-            streamWriter.WriteLine($"\t\tFontSize: {control.Font.Size}");
-            streamWriter.WriteLine();
+            streamWriter?.WriteLine($"{control.Name}:");
+            streamWriter?.WriteLine(PrettyPrint("Size", control.Size));
+            streamWriter?.WriteLine(PrettyPrint("Location", control.Location));
+            streamWriter?.WriteLine(PrettyPrint("ForeColour", control.ForeColor));
+            streamWriter?.WriteLine(PrettyPrint("BackColour", control.BackColor));
+            streamWriter?.WriteLine(PrettyPrint("Font", control.Font));
+
+            streamWriter?.WriteLine();
         }
-        streamWriter.WriteLine();
-        streamWriter.WriteLine();
+        streamWriter?.WriteLine();
+    }
+
+    private static string FormatSize(Size size) => $"[{size.Width}, {size.Height}]";
+    private static string FormatLocation(Point location) => $"[{location.X}, {location.Y}]";
+
+    private static string PrettyPrint(string name, object value)
+    {
+        value = name switch
+        {
+            "Size" => FormatSize((Size)value),
+            "Location" => FormatLocation((Point)value),
+            "Font" => $"{((Font)value).Name}\n\t\tFont Size: {((Font)value).Size}",
+            _ => value
+        };
+
+        return $"\t\t{name}: {value}";
     }
 }
+
